08568769dcfba91feecdc9beefafacac87e64bd9
Oliver Leisalu <oliver@bolt.eu>
2023-01-30 11:12:01 +0200

    fixed wrong commit, now deletes happen before update/new

--- src/StateBufferForSlave.ts
+++ src/StateBufferForSlave.ts
@@ -21,44 +21,50 @@ export abstract class StateBufferForSlave<T> extends StateBuffer {
     protected abstract onDelete(index: number, obj: T | undefined): void;
 
     public sync(): boolean {
-        const handleObject = (changeIndex: number, objIndex: number, obj: T) => {
-            if (this.exists(objIndex)) {
-                if (obj && this.isSame(objIndex, obj)) {
-                    this.onUpdate(objIndex, obj);
-                } else {
-                    if (obj) {
-                        this.onDelete(objIndex, obj);
-                        this.objects[objIndex] = undefined;
-                        this.objectsSet.delete(obj);
-                    }
-                    const newObj = this.onNew(objIndex);
-                    this.objects[objIndex] = newObj;
-                    this.objectsSet.add(newObj);
-                    this.onUpdate(objIndex, newObj);
-                }
-            } else if (obj) {
-                this.onDelete(objIndex, obj);
-                this.objects[objIndex] = undefined;
-                this.objectsSet.delete(obj);
-            }
-            this.isDirtyBufferView[objIndex] = StateBuffer.CLEAN;
-            this.changesBufferView[changeIndex] = 0;
-        };
         this.lock();
         let hadChanges;
         if (this.isInitialized === false) {
             this.isInitialized = true;
             hadChanges = true;
             const noOfObjects = this.controlBufferView[StateBufferForSlave.NO_OF_OBJECTS_INDEX];
-            for (let i = 0; i < noOfObjects; i++) {
-                handleObject(i, i, undefined);
+            for (let objIndex = 0; objIndex < noOfObjects; objIndex++) {
+                const newObj = this.onNew(objIndex);
+                this.objects[objIndex] = newObj;
+                this.objectsSet.add(newObj);
+                this.onUpdate(objIndex, newObj);
+
+                this.isDirtyBufferView[objIndex] = StateBuffer.CLEAN;
+                this.changesBufferView[objIndex] = 0;
             }
         } else {
             const noOfChanges = this.controlBufferView[StateBufferForSlave.NO_OF_DIRTY_OBJECTS_INDEX];
             hadChanges = noOfChanges > 0;
-            for (let i = 0; i < noOfChanges; i++) {
-                const index = this.changesBufferView[i];
-                handleObject(i, index, this.objects[index]);
+            for (let changeIndex = 0; changeIndex < noOfChanges; changeIndex++) {
+                const objIndex = this.changesBufferView[changeIndex];
+                const obj = this.objects[objIndex];
+                if (obj) {
+                    const exists = this.exists(objIndex);
+                    if (!exists || exists && !this.isSame(objIndex, obj)) {
+                        this.onDelete(objIndex, obj);
+                        this.objects[objIndex] = undefined;
+                        this.objectsSet.delete(obj);
+                    }
+                }
+            }
+            for (let changeIndex = 0; changeIndex < noOfChanges; changeIndex++) {
+                const objIndex = this.changesBufferView[changeIndex];
+                if (this.exists(objIndex)) {
+                    let obj = this.objects[objIndex];
+                    if (!obj) {
+                        obj = this.onNew(objIndex)
+                        this.objects[objIndex] = obj;
+                        this.objectsSet.add(obj);
+                    }
+                    this.onUpdate(objIndex, obj);
+                }
+
+                this.isDirtyBufferView[objIndex] = StateBuffer.CLEAN;
+                this.changesBufferView[changeIndex] = 0;
             }
             const noOfObjects = this.controlBufferView[StateBufferForSlave.NO_OF_OBJECTS_INDEX];
             if (noOfObjects < this.objects.length) {
